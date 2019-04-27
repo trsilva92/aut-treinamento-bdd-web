59779d926cd9f2cbcbcba706c6024add4af7039f
Tiago Ribeiro da Silva <tiago.ribeiro@inmetrics.com.br>
2019-04-27 12:56:41 -0300

    curso celso

--- src/test/java/curso/treinamento/runners/RunnerTest.java
+++ src/test/java/curso/treinamento/runners/RunnerTest.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = {"src/test/resources/features"},
         glue = {"curso.treinamento.steps", "curso.treinamento.setup"},
         snippets = SnippetType.CAMELCASE,
-        tags = {"@Registrar"})
+        tags = {""})
 
 public class RunnerTest {
 
